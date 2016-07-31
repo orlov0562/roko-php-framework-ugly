e8a8c951978f9e9b286716d90816c6632ea159ff
Vitaliy S. Orlov <orlov0562@gmail.com>
2016-07-31 14:08:04 +0300

    Error stack trace overrided

--- roko/flight.php
+++ roko/flight.php
@@ -155,4 +155,74 @@
     Flight::map('_files', function($varPath=null, $default=null){
         return Flight::_arr($_FILES, $varPath, $default);
     });
+    
+    
+    Flight::map('error', function($e){
+        $getTraceString = function($exception){
+            $rtn = "";
+            $count = 0;
+            foreach ($exception->getTrace() as $frame) {
+                $args = "";
+                if (isset($frame['args'])) {
+                    $args = array();
+                    foreach ($frame['args'] as $arg) {
+                        if (is_string($arg)) {
+                            $args[] = "'" . $arg . "'";
+                        } elseif (is_array($arg)) {
+                            $args[] = "Array";
+                        } elseif (is_null($arg)) {
+                            $args[] = 'NULL';
+                        } elseif (is_bool($arg)) {
+                            $args[] = ($arg) ? "true" : "false";
+                        } elseif (is_object($arg)) {
+                            $args[] = get_class($arg);
+                        } elseif (is_resource($arg)) {
+                            $args[] = get_resource_type($arg);
+                        } else {
+                            $args[] = $arg;
+                        }
+                    }
+                    $args = join(", ", $args);
+                }
+                $current_file = "[internal function]";
+                if(isset($frame['file']))
+                {
+                    $current_file = $frame['file'];
+                }
+                $current_line = "";
+                if(isset($frame['line']))
+                {
+                    $current_line = $frame['line'];
+                }
+                $rtn .= sprintf( "#%s %s(%s): %s(%s)\n",
+                        $count,
+                        $current_file,
+                        $current_line,
+                        $frame['function'],
+                        $args );
+                $count++;
+            }
+            return $rtn;                
+        };
+        
+        $msg = sprintf('<h1>500 Internal Server Error</h1>'.
+            '<h3>%s (%s)</h3>'.
+            '<pre>%s</pre>',
+            $e->getMessage(),
+            $e->getCode(),
+            $getTraceString($e)
+        );
 
+        try {
+            $this->response(false)
+                ->status(500)
+                ->write($msg)
+                ->send();
+        }
+        catch (\Throwable $t) {
+            exit($msg);
+        }
+        catch (\Exception $ex) {
+            exit($msg);
+        }
+    });
