a9ff9bdb9728d2b728ed9bfbddd380b21fd45119
Vitaliy S. Orlov <orlov0562@gmail.com>
2016-07-31 14:39:29 +0300

    Charset fixed for pdo connection

--- app/config/db.php
+++ app/config/db.php
@@ -12,6 +12,7 @@
                 'connection_string' => 'mysql:host=localhost;dbname=roko',
                 'username' => 'mysql',
                 'password' => 'mysql',
+                'driver_options' => [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'],
             ],
             'sqlite' => [
                 'autoload' => false,
